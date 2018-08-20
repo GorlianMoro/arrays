5638c57df354877e6b71f6ca1dd292d6fe028260
GorlianMoro <legckostupov.maxim@yandex.ru>
2018-08-20 11:04:17 +0300

    arrays ver. 1.2

--- index.php
+++ index.php
@@ -11,53 +11,17 @@ foreach ($animals as $reg)
   {
     if (strlen($beast) > 8)
     {
-      print_r($beast) . "<br>";
       $beastr[] = $beast;
     }
   }
 }
 
-print_r($beastr);
-/*  foreach ($animals as $region) {
-  $beast = array();
-  foreach ($region as $animal )
-  {
-    if (strlen($animal) > 8)
-    {
-      $bread = array($animal);
-    //  $arraybeast = array_fill(0, 1, $animal);
-    //  foreach ($arraybeast as $anim) {
-    //    $arraybeastes = array_merge($arraybeast, $anim);
-    //    print_r($arraybeastes);
-    //    echo $arraybeastes . "<br>";
-    //  }
-    $bear = explode(',' ,$animal);
-    $beast = array_merge($beast, $bear);
-  }
-    var_dump($bread) ;
-    //echo $bread;
-    echo "1 <br>";
-  }
-} */
+shuffle($beastr);
+foreach ($beastr as $fant_animal)
+{
+  //echo $fant_animal . "<br>";
+  $expanim = explode(" ", $fant_animal);
+  print_r ($expanim);
+}
 
-//$beasts = ['Canis lupus', 'Castor fiber', 'Alces alces', 'Sus scrofa', 'Diceros bicornis', 'Panthera leo', 'Eudorcas thomsonii', 'Equus grevyi'];
-
-//$monster = implode("<br>", $beasts);
-
-//$name = ['Canis', 'Castor', 'Alces', 'Sus', 'Diceros', 'Panthera', 'Eudorcas', 'Equus'];
-//$result = ['Sus', 'Diceros', 'Eudorcas', 'Equus', 'Canis', 'Panthera', 'Castor', 'Alces'];
-//$fantasy_name = str_replace($name, $result, $monster);
-
-//$type = ['lupus', 'fiber', 'alces', 'scrofa', 'bicornis', 'leo', 'thomsonii', 'grevyi'];
-//$res = ['thomsonii', 'grevyi', 'scrofa', 'alces', 'lupus', 'fiber', 'bicornis', 'leo'];
-//$fantasy_type = str_replace($type, $res, $monster);
-
-//$fantasy_animals = ['Fantasy place' => [$monster]];
-
-//foreach ($fantasy_animals as $region => $animal) {
-  //echo '<h2>' . $region, '</h2>';
-  //foreach ($animal as $monsters) {
-    //echo $monsters, '<br>';
-//  }
-//}
  ?>
