75e613b133b52bf89f58a4d3b6028e02bf39f342
Maxim <legckostupov.maxim@yandex.ru>
2018-08-22 19:11:51 +0300

    arrays ver. 1.31

--- index.php
+++ index.php
@@ -2,9 +2,7 @@
 $animals = array(
 'Eurasia' => array('Canis lupus', 'Castor fiber', ' Alces alces', 'Sus scrofa ', 'Folivora', 'Panthera'),
 'Africa' => array('Diceros bicornis', 'Panthera leo', 'Eudorcas thomsonii', 'Equus grevyi', 'Gorilla'));
-
 $beastr = array();
-
 foreach ($animals as $reg)
 {
   foreach ($reg as $beast)
@@ -15,33 +13,26 @@ foreach ($animals as $reg)
     }
   }
 }
-
+$expan = array();
+$expan2 = array();
 foreach ($beastr as $fant_animal)
 {
   $expanim = explode(" ", $fant_animal);
   $expan[] = $expanim[0];
   $expan2[] = $expanim[1];
 }
-shuffle($expan);
-shuffle($expan2);
 
+shuffle($expan2);
 foreach ($expan as $anim)
 {
-  $bea = $anim;
   foreach ($expan2 as $anim2)
   {
-    $bea2 = $anim2;
-    $sumbea = $bea . $bea2;
+    $sumbea = $anim . " " . $anim2;
     $fantabea[] = $sumbea;
   }
 }
-
 foreach ($fantabea as $beast)
 {
-
   echo $beast . "<br>";
 }
-
-
-
  ?>
